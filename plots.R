1d9d4851d383fd35055a76fe558a694832851f99
danbm94 <danbustillo94@gmail.com>
2021-08-23 09:49:39 +0200

    Added plots and datasets from exercises

--- plots.R
+++ plots.R
@@ -2,22 +2,99 @@
 library(tidyverse)
 data <- read.csv("Dataset/API_EN.ATM.CO2E.PC_DS2_en_csv_v2_2708833.csv",skip = 3, header = T)
 data <- data[,-c(2:4)]
-ger <- data[56,]
-ger <- pivot_longer(data = ger, cols = !1,names_to = "Germany" )
-series <- ts(data = ger$value)
-plot(series,type="l") 
+#Europe
 eur <- data[74,]
 eur <- pivot_longer(eur,cols = !1)
 eur <- eur[-62,]
 eur$name <- gsub(pattern = "X",replacement = "", eur$name)
 eur$date <- as.Date(eur$name,format = "%Y")
 ggplot(data=eur)+geom_line(aes(x=date, y=value)) + 
-  theme_minimal()
-eur <- ts(data=eur$value)
-log_eur <- ts(data=log(eur$value))
-plot(eur, type='l')
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions in the European Union", subtitle = "in metric tons per capita from 1960 to 2020")
 
-log_eur <- log(eur$value)
-ts_l_eur <- ts(log_eur)
-plot(ts_l_eur, type='l')
-max(ts_l_eur,na.rm = TRUE)
+
+# US
+us <- data[data$Country.Name=="United States",] %>% 
+  pivot_longer(cols = !c(1:4)) %>% 
+  select(name, value) %>% 
+  na.omit()
+us$name <- gsub(pattern="X", replacement = "", us$name) 
+us$date <- as.Date(us$name,format = "%Y")
+
+ggplot(data=us)+ geom_line(aes(x=date,y= value))+
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions in the United States", subtitle = "in metric tons per capita from 1960 to 2018")
+
+
+# China
+
+chi <- data[data$Country.Name=="China",] %>% 
+  pivot_longer(cols = !c(1:4)) %>% 
+  select(name, value) %>% 
+  na.omit()
+chi$name <- gsub(pattern="X", replacement = "", chi$name) 
+chi$date <- as.Date(chi$name,format = "%Y")
+
+ggplot(data=chi)+ geom_line(aes(x=date,y= value))+
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions in China", subtitle = "in metric tons per capita from 1960 to 2018")
+
+
+# Brazil
+br <- data[data$Country.Name=="Brazil",] %>% 
+  pivot_longer(cols = !c(1:4)) %>% 
+  select(name, value) %>% 
+  na.omit()
+br$name <- gsub(pattern="X", replacement = "", br$name) 
+br$date <- as.Date(br$name,format = "%Y")
+
+ggplot(data=br)+ geom_line(aes(x=date,y= value))+
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions in Brazil", subtitle = "in metric tons per capita from 1960 to 2018")
+
+
+# India
+
+ind <- data[data$Country.Name=="India",] %>% 
+  pivot_longer(cols = !c(1:4)) %>% 
+  select(name, value) %>% 
+  na.omit()
+ind$name <- gsub(pattern="X", replacement = "", ind$name) 
+ind$date <- as.Date(ind$name,format = "%Y")
+
+ggplot(data=ind)+ geom_line(aes(x=date,y= value))+
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions in India", subtitle = "in metric tons per capita from 1960 to 2018")
+
+
+## All combined
+
+ggplot()+
+  geom_line(aes(x=date, y=value, colour= "European Union"), data=eur, col="darkblue")+
+  geom_line(aes(x=date, y=value), data=us, col="blue")+
+  geom_line(aes(x=date, y=value), data=chi, col="red")+
+  geom_line(aes(x=date, y=value), data=br, col="green")+
+  geom_line(aes(x=date, y=value), data=ind, col="orange")+
+  theme_classic() +
+  xlab(label = "Year")+
+  ylab("CO2 Emissions (metric tons per capita)")+
+  ggtitle(label = "CO2 Emissions", subtitle = "in metric tons per capita from 1960 to 2018")
+
+
+  
+  
+  
+  
+  
+  
+  
